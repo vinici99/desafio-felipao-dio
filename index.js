202cf462be0db439a9dfd3f7b1bf30563c284236
vinici99 <viniciusgomes0413@gmail.com>
2024-01-09 23:47:55 -0300

    desafio felipao

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-efjkwS
+let nome = "Vinicius"
+let xp = 8500
+let nivel = ""
+
+if (xp <= 1000){
+  nivel = "Ferro" 
+} else if (xp <= 2000){
+  nivel = "Bronze" 
+} else if (xp <= 6000){
+  nivel = "Prata"  
+} else if (xp <= 7000){
+  nivel = "Ouro"  
+} else if (xp <= 8000){
+  nivel = "Platina"  
+} else if (xp <= 9000){
+  nivel = "Ascendente"
+} else if (xp <= 10000){
+  nivel = "Imortal"  
+} else if (xp >=10001){
+  nivel = "Radiante"  
+} 
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
